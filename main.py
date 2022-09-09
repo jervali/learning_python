c668d4bfd0ca7e5599d5ec89f1d2d08e7c065f71
jervali <jervazliz@gmail.com>
2022-09-09 01:45:11 -0700

    Updated

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print(200)
+print()
+print("Suma de Gauss")
+print()
+
+# Sumar los numeros del 0 al 100
+
+total = 0
+for num in range(101):
+    total = total + num
+    print(total) # Imprime cada suma individual
+print(total) #Imprime resultado total
